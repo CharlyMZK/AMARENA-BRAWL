1dc12191982e43b79c951815a671069978a1b985
Arcahyxe <dell.quentin@gmail.com>
2017-03-03 23:57:04 +0100

    Adding menu

--- app/src/main/java/com/amarena/rss/amarena_brawl/MenuActivity.java
+++ app/src/main/java/com/amarena/rss/amarena_brawl/MenuActivity.java
@@ -1,13 +1,10 @@
 package com.amarena.rss.amarena_brawl;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import mehdi.sakout.fancybuttons.FancyButton;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -18,13 +15,74 @@ public class MenuActivity extends AppCompatActivity {
         initButton();
     }
 
-    private void initButton(){
-        FancyButton btn_menu_pvp = (FancyButton) this.findViewById(R.id.btn_menu_pvp);
-        btn_menu_pvp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getBaseContext(), "PEDALE", Toast.LENGTH_SHORT);
-            }
-        });
+    /**
+     * Permet d'initialiser les boutons
+     */
+    private void initButton() {
+        Button btnMenuPvp = (Button) this.findViewById(R.id.btnMenuPvp);
+        Button btnMenuPvb = (Button) this.findViewById(R.id.btnMenuPvb);
+        Button btnMenuCustomisation = (Button) this.findViewById(R.id.btnMenuCustomisation);
+        Button btnMenuSettings = (Button) this.findViewById(R.id.btnMenuSettings);
+
+        if (btnMenuPvp != null)
+            btnMenuPvp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onClickBtnMenuPvp();
+                }
+            });
+        if (btnMenuPvb != null)
+            btnMenuPvb.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onClickBtnMenuPvb();
+                }
+            });
+        if (btnMenuCustomisation != null)
+            btnMenuCustomisation.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onClickBtnMenuCustomisation();
+                }
+            });
+        if (btnMenuSettings != null)
+            btnMenuSettings.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onClickBtnMenuSettings();
+                }
+            });
+    }
+
+    /**
+     * Actions pour le bouton du menu pvp
+     */
+    private void onClickBtnMenuPvp() {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Actions pour le bouton du menu pvb
+     */
+    private void onClickBtnMenuPvb() {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Actions pour le bouton du menu de personnalisation
+     */
+    private void onClickBtnMenuCustomisation() {
+        Intent intent = new Intent(this, CustomisationActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Actions pour le bouton du menu des paramètres
+     */
+    private void onClickBtnMenuSettings() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
